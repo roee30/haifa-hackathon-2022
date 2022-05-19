8b4d02cd23552d739789aa53b475dce0af516690
Roee Nizan <roeen30@gmail.com>
2022-05-19 07:17:56 +0300

    changed line mode to file mode

--- camel.py
+++ camel.py
@@ -12,32 +12,31 @@ out_dir = Path("out")
 
 def translate(path: Path):
     out_path = out_dir / path.relative_to("data")
-    done_file = out_path.with_name(f"{out_path.name}.done")
-    if done_file.exists():
+    if out_path.exists():
         print(f"skipping: {out_path}")
         return
-    print(f"processing:: {path} -> {out_path}")
-    with path.open() as in_file, out_path.open("w") as out_file:
-        for i, line in enumerate(in_file):
-            print(f"line {i}\r")
-            mle = MLEDisambiguator.pretrained()
+    print(f"processing: {path}")
+    text = path.read_text()
 
-            # The disambiguator expects pre-tokenized text
-            sentence = simple_word_tokenize(line)
+    mle = MLEDisambiguator.pretrained()
 
-            disambig = mle.disambiguate(sentence)
+    # The disambiguator expects pre-tokenized text
+    sentence = simple_word_tokenize(text)
 
-            # For each disambiguated word d in disambig, d.analyses is a list of analyses
-            # sorted from most likely to least likely. Therefore, d.analyses[0] would
-            # be the most likely analysis for a given word. Below we extract different
-            # features from the top analysis of each disambiguated word into seperate lists.
-            ana = [d.analyses[0].analysis for d in disambig]
-            words = [(a["diac"], a["lex"], a["pos"]) for a in ana]
-            out_path.parent.mkdir(parents=True, exist_ok=True)
-            for diac, lex, part_of_speech in words:
-                if part_of_speech not in ["punc", "digit"]:
-                    out_file.write(f"{lex} ")
-    done_file.touch()
+    disambig = mle.disambiguate(sentence)
+
+    # For each disambiguated word d in disambig, d.analyses is a list of analyses
+    # sorted from most likely to least likely. Therefore, d.analyses[0] would
+    # be the most likely analysis for a given word. Below we extract different
+    # features from the top analysis of each disambiguated word into seperate lists.
+    ana = [d.analyses[0].analysis for d in disambig]
+    words = [(a["diac"], a["lex"], a["pos"]) for a in ana]
+    print(f"writing: {path} -> {out_path}")
+    out_path.parent.mkdir(parents=True, exist_ok=True)
+    with out_path.open("w") as f:
+        for diac, lex, part_of_speech in words:
+            if part_of_speech not in ["punc", "digit"]:
+                f.write(f"{lex} ")
 
 
 def main():
