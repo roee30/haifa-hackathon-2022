f003f3acee7250b0d9c8bb71ed7ef5890d73f978
Roee Nizan <roeen30@gmail.com>
2022-05-20 21:51:38 +0300

    initial commit

--- bag_of_words.py
+++ bag_of_words.py
@@ -1,21 +1,22 @@
 import pandas as pd
 from sklearn.feature_extraction.text import CountVectorizer
 from pathlib import Path
- 
-data_list = list()
-clean_path = Path("TreeOfKnowledge\degenerated_arabic")
- 
-for ix ,file in enumerate(clean_path.rglob("*.txt")):
-    print(ix, file)
-    with open(file, mode='r', encoding="utf-8") as f:
-        data = f.read()
-    data_list.append(data)        
-text = data_list
- 
- 
+from main import non_hebrew as english, clean, compose
+
+pipe = compose(
+    (english.sub, ""),
+    (clean.sub, ""),
+)
+
+clean_path = Path("data/degenerated_arabic")
+text = [pipe(file.read_text()) for file in clean_path.rglob("*.txt")]
+
+
+print("vectorizing")
 coun_vect = CountVectorizer()
 count_matrix = coun_vect.fit_transform(text)
 count_array = count_matrix.toarray()
-df = pd.DataFrame(data=count_array,columns = coun_vect.get_feature_names())
+df = pd.DataFrame(data=count_array, columns=coun_vect.get_feature_names())
 print(df)
- 
+df.to_csv("out/bag_of_words_degenerated.csv", encoding="utf-8")
+print("done")
--- camel.py
+++ camel.py
@@ -2,19 +2,21 @@ from pathlib import Path
 
 from camel_tools.tokenizers.word import simple_word_tokenize
 from camel_tools.disambig.mle import MLEDisambiguator
+from openiti.helper.ara import denoise
 from tqdm import tqdm
 
 path_ = "data/openITI_clean_text/0595IbnRushdHafid.MaqalaFiBudhur.DARE00016-ara1.txt"
-in_dir = Path("data/openITI_clean_text")
-# in_dir = Path("data/degenerated_arabic")
+# in_dir = Path("data/openITI_clean_text")
+in_dir = Path("data/degenerated_arabic")
 out_dir = Path("out")
+out2 = Path("out2")
 
 
 def translate(path: Path):
     out_path = out_dir / path.relative_to("data")
     if out_path.exists():
         print(f"skipping: {out_path}")
-        return
+        return out_path
     print(f"processing: {path}")
     text = path.read_text()
 
@@ -36,7 +38,17 @@ def translate(path: Path):
     with out_path.open("w") as f:
         for diac, lex, part_of_speech in words:
             if part_of_speech not in ["punc", "digit"]:
-                f.write(f"{lex} ")
+                f.write(f"{denoise(lex)} ")
+    return out_path
+
+
+def denoise_(path: Path):
+    out_path = out2 / path.relative_to(out_dir)
+    if out_path.exists():
+        print(f"skipping denoise: {out_path}")
+    out_path.parent.mkdir(parents=True, exist_ok=True)
+    out_path.write_text(denoise(path.read_text()))
+    print(f"denoised: {out_path}")
 
 
 def main():
